e1d310f100435ee9b5ccbe6b1d5032e6c9a95a99
J. Michael Brown <jmichaelbrown8@gmail.com>
2022-01-10 22:47:10 -0800

    Added details for each section, added a license generator based on the user's selection

--- generateReadme.js
+++ generateReadme.js
@@ -13,6 +13,11 @@ module.exports = {
         }) =>
         
 `# ${projName}
+${license === 'Apache' ? '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)' : ''}
+${license === 'GNU' ? '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)' : ''}
+${license === 'MIT' ? '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)' : ''}
+${license === 'Creative Commons' ? '[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)' : ''}
+${license === 'The Unlicense' ? '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)' : ''}
 
 ##### Table of Contents
 ${projDesc ? "* [Description](#description)" : ""}
@@ -22,18 +27,36 @@ ${usage ? "* [Usage](#usage)" : ""}
 ${tests ? "* [Tests](#tests)" : ""}
 ${contributing ? "* [Contributing](#contributing)" : ""}
 ${github || email ? "* [Questions](#questions)" : ""}
-${license ? "* [License](#license)" : ""}
+${license !== 'No License' ? "* [License](#license)" : ""}
 
 ${projDesc ? `## Description` : ''}
+${projDesc}
 
 ${ prereq ? `## Prerequisites` : "" }
 ${ prereq ? prereq.split(",").reduce((x, y) => `${x}* ${y}\n`, '') : "" }
 
-${install ? "## Installation" : ""}
+${install && "## Installation"}
+${install}
+
 ${usage ? "## Usage" : ""}
+${usage}
+
 ${tests ? "## Tests" : ""}
+${tests}
+
 ${contributing ? "## Contributing" : ""}
+${contributing}
+
 ${github || email ? "## Questions" : ""}
-${license ? "## License" : ""}
+${github || email ? `Reach out to ${github || email}` : ""}
+
+${license !== 'No License' ? "## License" : ""}
+${license === 'Apache' ? '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)' : ''}
+${license === 'GNU' ? '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)' : ''}
+${license === 'MIT' ? '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)' : ''}
+${license === 'Creative Commons' ? '[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)' : ''}
+${license === 'The Unlicense' ? '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)' : ''}
+
+${license !== 'No License' ? 'View the license [here](./LICENSE)' : ''}
 `,
 };
--- index.js
+++ index.js
@@ -1,19 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { generateReadme } = require('./generateReadme')
-
-/*
-* Project Name,
-GitHub,
-* Description, 
-Table of Contents, (generated)
-* Installation, 
-* Usage, 
-License, (list)
-* Contributing, 
-Tests, 
-and Questions (user's email address)
-*/
+const { generateReadme } = require('./generateReadme');
+const { generateLicense } = require('./generateLicense');
 
 inquirer
     .prompt([
@@ -72,14 +60,26 @@ inquirer
     .then( 
         answers => {
 
+            // Create a build directory so the generated README.md doesn't clash with this script's README.md
             fs.mkdir('./build/', { recursive: true }, err => err ? console.error(err) : console.log('Created ./build/ folder'));
             
+            // Write out the README.md to the new directory
             fs.writeFile(
                 './build/README.md', 
                 generateReadme(answers), 
                 err => 
                     err ? console.error(err) : console.log('Completed ./build/README.md')
             )
+
+            // If a license is specified, write out a license file too
+            if (answers.license) {
+                fs.writeFile(
+                    './build/LICENSE',
+                    generateLicense(answers),
+                    err =>
+                        err ? console.error(err) : console.log('Completed ./build/LICENSE')
+                )
+            }
         }
 
     )
